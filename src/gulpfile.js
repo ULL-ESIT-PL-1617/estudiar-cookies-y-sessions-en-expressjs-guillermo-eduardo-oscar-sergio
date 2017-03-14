b92981511f34dc83890b244f7648e7aa303f4f63
alu0100892833 <alu0100892833@ull.edu.es>
2017-03-14 19:31:47 +0000

    Automatizando despliegue en GitHub Pages y GitBook (ambos funcionan).

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -12,31 +12,26 @@ gulp.task('build', function() {
 
 
 // Despliegue del libro en GitHub Pages
-var repositorio = require('../package.json').repository.url;
 var ghpages = require('gh-pages');
 
-gulp.task('deploy-gh-pages', function() {
-    return gulp.src('').pipe(
-        ghpages.publish('../gh-pages', {
-            repo: repositorio,
-            logger: function(m) {
-                console.error(m);
-            }
-        })
-    );
+gulp.task('deploy-gh-pages', ['build'], function() {
+    ghpages.publish('../gh-pages', {
+        repo: "https://github.com/ULL-ESIT-PL-1617/estudiar-cookies-y-sessions-en-expressjs-guillermo-eduardo-oscar-sergio.git",
+        logger: function(m) {
+            console.error(m);
+        }
+    });
 });
 
 
 // Despliegue del libro en GitBook
 gulp.task('deploy-gitbook', function() {
-    return gulp.src('').pipe(
-        ghpages.publish('../docs', {
-            branch: 'master',
-            repo: "https://git.gitbook.com/alu0100892833/estudiar-cookies-y-sessions-en-expressjs.git"
-        }, function(err) {
-             console.log(err);
-        })
-     );
+    ghpages.publish('../docs', {
+        branch: 'master',
+        repo: "https://git.gitbook.com/alu0100892833/estudiar-cookies-y-sessions-en-expressjs.git"
+    }, function(err) {
+         console.log(err);
+    });
 });
 
 
